5b623c2fe7bf503a03fa579e734bf030ddf841ef
Samid77 <dimaseptyanto@windowslive.com>
2020-05-07 12:49:03 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Delivery Prototype',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey[50],
+        primaryColor: Colors.deepOrangeAccent,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
