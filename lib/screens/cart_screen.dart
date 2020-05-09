7a1ca8d1d6081872775d6335fab3903a40aae1ea
Samid77 <dimaseptyanto@windowslive.com>
2020-05-09 14:35:47 +0700

    Finalize

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -52,13 +52,70 @@ class _CartScreenState extends State<CartScreen> {
                           ),
                           overflow: TextOverflow.ellipsis,
                         ),
+                        SizedBox(height: 10.0),
+                        Container(
+                          width: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            border: Border.all(
+                              width: 0.8,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  '-',
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w600
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 20.0,),
+                              Text(
+                                order.quantity.toString(),
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w600
+                                ),
+                              ),
+                              SizedBox(width: 20.0,),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  '+',
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w600
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
                       ],
                     ),
                   ),
-                )
+                ),
               ],
             ),
-          )
+          ),
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              'Rp.${order.quantity * order.food.price}',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -66,15 +123,71 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
+
+    double totalPrice = 0;
+    currentUser.cart.forEach((Order order) {
+      totalPrice += order.quantity * order.food.price;
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Cart (${currentUser.cart.length})'),
       ),
       body: ListView.separated(
+        itemCount: currentUser.cart.length + 1,
         itemBuilder: (BuildContext context, int index) {
-          Order order = currentUser.cart[index];
-          return _buildCartItem(order);
-          
+          if(index < currentUser.cart.length) {
+            Order order = currentUser.cart[index];
+            return _buildCartItem(order);
+          }
+          return Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Estimated Delivery Time: ',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '30 min',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Total Cost: ',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'Rp.${totalPrice.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.green[700]
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 80.0,)
+              ],
+            ),
+          );
           // return Container(
           //   height: 100.0,
           //   color: Colors.red,
@@ -86,8 +199,33 @@ class _CartScreenState extends State<CartScreen> {
             height: 1.0,
             color: Colors.grey,
           );
-        }, 
-        itemCount: currentUser.cart.length)
+        },
+      ),
+      bottomSheet: Container(
+        height: 100.0,
+        width: MediaQuery.of(context).size.width, 
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          boxShadow: [BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, -1),
+            blurRadius: 6.0,
+          )]
+        ),
+        child: Center(
+          child: FlatButton(
+            child: Text(
+              'Checkout',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ),
     );
   }
 }
